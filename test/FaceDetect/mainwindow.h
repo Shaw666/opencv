7c1a8782300987bfa93e1275f00e33cc8bbcccc2
xhp-lx <15323435028@163.com>
2022-06-12 22:53:37 +0800

    update

--- test/FaceDetect/mainwindow.h
+++ test/FaceDetect/mainwindow.h
@@ -3,13 +3,17 @@
 
 #include <QMainWindow>
 #include "opencv2/opencv.hpp"
+#include "opencv2/face.hpp"
 #include <string>
 #include <vector>
+#include <queue>
+#include <mutex>
+#include "ImageWidget.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui
 {
-    class MainWindow;
+class MainWindow;
 }
 QT_END_NAMESPACE
 
@@ -24,10 +28,18 @@ public:
 private:
     Ui::MainWindow *ui;
 
-    cv::CascadeClassifier face_cascade;   //定义人脸分类器
+    cv::CascadeClassifier face_cascade; //定义人脸分类器
+    cv::Ptr<cv::face::FaceRecognizer> model_;
+    std::unique_ptr<cv::VideoCapture> video_capture_;
+    std::queue<cv::Mat> frame_bgr_queue_;
+    std::mutex* pMutex_;
+    ImageWidget *m_pImageWidget;
 private:
-    void detect();
-    void read_csv(const std::string& filename, std::vector<cv::Mat>& images, std::vector<int>& labels, char separator = ';');
-    void findYou();
+    void detectFrame(cv::Mat &frame_bgr);
+    void detectImages();
+    void read_csv(const std::string &filename, std::vector<cv::Mat> &images, std::vector<int> &labels, char separator = ';');
+    void loadModule();
+    void FindYou(cv::Mat &testSample, int testLabel);
+    void VideoCapture();
 };
 #endif // MAINWINDOW_H
